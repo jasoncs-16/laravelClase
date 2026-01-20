eb97fb7cd1650b6b70e89fb6f34e0abacf720a50
jasoncs-16 <jasoncsotto16@gmail.com>
2026-01-20 21:53:14 +0000

    updates

--- resources/views/formulario_libros.blade.php
+++ resources/views/formulario_libros.blade.php
@@ -3,22 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <title>Gestión de libros</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-
-        <!-- Styles / Scripts -->
-        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-            @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @endif
     </head>
     <body class="">
         <form action="/" method="POST" enctype="multipart/form-data" class="flex flex-col">
-            @csrf
             <input type="text" name="nombre" placeholder="Nombre" class="text-center">
             <input type="number" name="edad" placeholder="Edad" class="text-center">
             <button type="submit" class="">Enviar</button>
